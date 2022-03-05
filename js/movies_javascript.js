852a6945a64cab8c79db4dcca9490042585a1ffc
Rohan Patel <rohanpatel4846@gmail.com>
2022-03-05 06:37:10 +0530

    Added default image for movies

--- js/movies_javascript.js
+++ js/movies_javascript.js
@@ -21,7 +21,12 @@ function Movies_Titles_Maniputlating(titles_data){
 
 function GetCardHTML(title, poster, imdbId, releaseYear, cardID, serverId){
     let cardHtml = "";
-    cardHtml += `<div class="card" onclick="OpenNormalLink('${cardID}','${serverId}')" data-bs-toggle="tooltip" data-bs-placement="bottom" title="${title}"><div class="cardPosterDiv"><img loading="lazy" src="${poster}" class="cardPoster"></div><span class="cardPrimary">${title}</span><div class="row cardSecondary"><div class="cardSecondaryOne col-6">${imdbId}</div><div class="cardSecondaryTwo col-6">${releaseYear}</div></div></div>`;
+    if(poster == "-"){
+        cardHtml += `<div class="card" onclick="OpenNormalLink('${cardID}','${serverId}')" data-bs-toggle="tooltip" data-bs-placement="bottom" title="${title}"><div class="cardPosterDiv"><img loading="lazy" src="https://lh3.googleusercontent.com/pw/AM-JKLVg3mwsai1C8nNsyBHJiUSzWtB3u6d1s_68o9v6xKKfDSv4m-Q79L2bDmZ1JrurGIdOJnWRduOgb2VFxQkvwnhjbjQUFckd_AB2yfnCra1hWznHTNtHDuYYR1z2Z48GR2nl2MocGUUG6xWJkX6YLWH0SA=w586-h879-no?authuser=0" class="cardPoster"></div><span class="cardPrimary">${title}</span><div class="row cardSecondary"><div class="cardSecondaryOne col-6">${imdbId}</div><div class="cardSecondaryTwo col-6">${releaseYear}</div></div></div>`;
+    }
+    else{
+        cardHtml += `<div class="card" onclick="OpenNormalLink('${cardID}','${serverId}')" data-bs-toggle="tooltip" data-bs-placement="bottom" title="${title}"><div class="cardPosterDiv"><img loading="lazy" src="${poster}" class="cardPoster"></div><span class="cardPrimary">${title}</span><div class="row cardSecondary"><div class="cardSecondaryOne col-6">${imdbId}</div><div class="cardSecondaryTwo col-6">${releaseYear}</div></div></div>`;
+    }
     return cardHtml;
 }
 
