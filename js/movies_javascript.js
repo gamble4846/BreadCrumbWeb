0628fd89423a4320a9a73b16439df9957c529f08
Rohan Patel <rohanpatel4846@gmail.com>
2022-03-06 22:57:34 +0530

    Completed filter, search and sorting for movies page and added breadcrumb logo in footer

--- js/movies_javascript.js
+++ js/movies_javascript.js
@@ -1,22 +1,29 @@
+var CurrentTitlesData = [];
+var CurrentTitlesDataToUse = [];
+
 $( document ).ready(function() {
     let db = new Localbase('BreadCrumb_Local_DB');
     db.collection('all_Data').get().then(all_Data => {
         all_Data = all_Data[0];
         titles_data = all_Data.Movies.Titles;
-        console.log(titles_data);
         Movies_Titles_Maniputlating(titles_data);
+        CreateVirtualSelect("genereSelect",null);
     });
 });
 
+function RemoveAllDivs(parentDivID){
+    $(parentDivID).children().remove();
+}
+
 function Movies_Titles_Maniputlating(titles_data){
-    let rowHTML = "";
     for(let i=0; i<titles_data.length; i++){
         for(let j=0; j<(titles_data[i].DATA).length; j++){
-            let cardHtml = GetCardHTML(titles_data[i].DATA[j].Movies_MainName, titles_data[i].DATA[j].Movies_Ver_Poster, titles_data[i].DATA[j].Movie_IMDB_ID, titles_data[i].DATA[j].Movies_ReleaseYear, titles_data[i].DATA[j].Movies_Id, titles_data[i].Server_ID);
-            rowHTML += `<div class="col-md-2 mt-5 col-sm-4 col-6">${cardHtml}</div>`;
+            titles_data_obj = titles_data[i].DATA[j];
+            titles_data_obj.Server_ID = titles_data[i].Server_ID;
+            CurrentTitlesData.push(titles_data_obj);
         }
     }
-    $("#all_movies_a_z").html(rowHTML);
+    UpdateMovies();
 }
 
 function GetCardHTML(title, poster, imdbId, releaseYear, cardID, serverId){
@@ -30,7 +37,174 @@ function GetCardHTML(title, poster, imdbId, releaseYear, cardID, serverId){
     return cardHtml;
 }
 
-
 function OpenNormalLink(id, serverId){
     window.open(`movie_openedMovie.html?movie_id=${id}&movie_serverid=${serverId}`,"_self");
+}
+
+function CreateVirtualSelect(elementID, options_){
+    elementID = "#"+elementID;
+
+    VirtualSelect.init({
+        ele: elementID,
+        search: true,
+        multiple: true,
+        showSelectedOptionsFirst: true,
+    });
+
+    var options = [];
+
+    let AllGeneres = [];
+    CurrentTitlesDataToUse.forEach(row => {
+        let tempGenere = String(row.Movies_Genre).split(",");
+        tempGenere.forEach(element => {
+            AllGeneres.push(element.replaceAll(" ", ""));
+        });
+    });
+    AllGeneres = AllGeneres.filter(onlyUnique);
+    AllGeneres.sort();
+    AllGeneres.forEach(genere => {
+        let genereObj = {};
+        genereObj.label = genere;
+        genereObj.value = genere;
+        options.push(genereObj);
+    });
+      
+    document.querySelector(elementID).setOptions(options);
+}
+
+function onlyUnique(value, index, self) {
+    return self.indexOf(value) === index;
+}
+
+function UpdateMovies(){
+    CurrentTitlesDataToUse = CurrentTitlesData;
+    RemoveAllDivs("#all_movies");
+    let rowHTML = "";
+    filterSort();
+    filterGenere();
+    filterSearch();
+
+    CurrentTitlesDataToUse.forEach(row => {
+        let cardHtml = GetCardHTML(row.Movies_MainName, row.Movies_Ver_Poster, row.Movie_IMDB_ID, row.Movies_ReleaseYear, row.Movies_Id, row.Server_ID);
+        rowHTML += `<div class="col-md-2 mt-5 col-sm-4 col-6">${cardHtml}</div>`;
+    });
+    $("#all_movies").html(rowHTML);
+}
+
+function ChangeSortingOrder(node){
+    $("#sortingTypeDIV").attr("data-select-btn", $(node).attr("data-value"));
+    UpdateMovies();
+}
+
+function filterSearch(){
+    let SearchTerm = ($("#movieSearch").val()).toUpperCase();
+    let filteredList = [];
+    CurrentTitlesDataToUse.forEach(row => {
+        let mainName = String(row.Movies_MainName).toUpperCase();
+        let secondaryName = String(row.Movies_AltNames).toUpperCase();
+        let imdbID = String(row.Movie_IMDB_ID).toUpperCase();
+        if(mainName.includes(SearchTerm) || secondaryName.includes(SearchTerm) || imdbID.includes(SearchTerm)){
+            filteredList.push(row);
+        }
+    });
+
+    CurrentTitlesDataToUse = filteredList;
+}
+
+function filterSort(){
+    let titleObj = {};
+    let SortingOrder = $("#sortingTypeDIV").attr("data-select-btn");
+    
+    switch(SortingOrder){
+        case "addDesc":
+            for (let i = 0; i < CurrentTitlesDataToUse.length; i++) {
+                for (let j = i+1; j < CurrentTitlesDataToUse.length; j++) {
+                    if(CurrentTitlesDataToUse[i].Movies_Id < CurrentTitlesDataToUse[j].Movies_Id){
+                        titleObj = CurrentTitlesDataToUse[i];
+                        CurrentTitlesDataToUse[i] = CurrentTitlesDataToUse[j];
+                        CurrentTitlesDataToUse[j] = titleObj;
+                    }
+                }
+            }
+            break;
+        case "addAsc":
+            for (let i = 0; i < CurrentTitlesDataToUse.length; i++) {
+                for (let j = i+1; j < CurrentTitlesDataToUse.length; j++) {
+                    if(CurrentTitlesDataToUse[i].Movies_Id > CurrentTitlesDataToUse[j].Movies_Id){
+                        titleObj = CurrentTitlesDataToUse[i];
+                        CurrentTitlesDataToUse[i] = CurrentTitlesDataToUse[j];
+                        CurrentTitlesDataToUse[j] = titleObj;
+                    }
+                }
+            }
+            break;
+        case "azAsc":
+            for (let i = 0; i < CurrentTitlesDataToUse.length; i++) {
+                for (let j = i+1; j < CurrentTitlesDataToUse.length; j++) {
+                    if(CurrentTitlesDataToUse[i].Movies_MainName > CurrentTitlesDataToUse[j].Movies_MainName){
+                        titleObj = CurrentTitlesDataToUse[i];
+                        CurrentTitlesDataToUse[i] = CurrentTitlesDataToUse[j];
+                        CurrentTitlesDataToUse[j] = titleObj;
+                    }
+                }
+            }
+            break;
+        case "azDesc":
+            for (let i = 0; i < CurrentTitlesDataToUse.length; i++) {
+                for (let j = i+1; j < CurrentTitlesDataToUse.length; j++) {
+                    if(CurrentTitlesDataToUse[i].Movies_MainName < CurrentTitlesDataToUse[j].Movies_MainName){
+                        titleObj = CurrentTitlesDataToUse[i];
+                        CurrentTitlesDataToUse[i] = CurrentTitlesDataToUse[j];
+                        CurrentTitlesDataToUse[j] = titleObj;
+                    }
+                }
+            }
+            break;
+        case "ryAsc":
+            for (let i = 0; i < CurrentTitlesDataToUse.length; i++) {
+                for (let j = i+1; j < CurrentTitlesDataToUse.length; j++) {
+                    if(CurrentTitlesDataToUse[i].Movies_ReleaseYear > CurrentTitlesDataToUse[j].Movies_ReleaseYear){
+                        titleObj = CurrentTitlesDataToUse[i];
+                        CurrentTitlesDataToUse[i] = CurrentTitlesDataToUse[j];
+                        CurrentTitlesDataToUse[j] = titleObj;
+                    }
+                }
+            }
+            break;
+        case "ryDesc":
+            for (let i = 0; i < CurrentTitlesDataToUse.length; i++) {
+                for (let j = i+1; j < CurrentTitlesDataToUse.length; j++) {
+                    if(CurrentTitlesDataToUse[i].Movies_ReleaseYear < CurrentTitlesDataToUse[j].Movies_ReleaseYear){
+                        titleObj = CurrentTitlesDataToUse[i];
+                        CurrentTitlesDataToUse[i] = CurrentTitlesDataToUse[j];
+                        CurrentTitlesDataToUse[j] = titleObj;
+                    }
+                }
+            }
+            break;
+    }
+}
+
+function filterGenere(){
+    console.log(CurrentTitlesDataToUse);
+    let Selectedgenere = $("#genereSelect").val();
+    let filteredData = [];
+    if(Selectedgenere != null && Selectedgenere.length != 0){
+        CurrentTitlesDataToUse.forEach(row => {
+            let flag = false;
+            Selectedgenere.forEach(genere => {
+                if(String(row.Movies_Genre).includes(genere)){
+                    flag = true;
+                }
+            });
+            if(flag){
+                filteredData.push(row);
+            }
+        });
+        CurrentTitlesDataToUse = filteredData;
+        console.log(CurrentTitlesDataToUse);
+    }
+    else{
+        console.log("Empty");
+    }
 }
